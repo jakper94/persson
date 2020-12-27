8bc4703b6325fc250b6f3c09f7e13539634346a7
Jakob Persson <jakob.j.persson@hotmail.com>
2020-12-27 10:33:55 +0100

    Add files via upload

---
+++ precache-manifest.59745f4973fb70465f1be1d3632d0e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c13ec4044b1b43ba5b3514a13fc3b26",
+    "url": "/persson/index.html"
+  },
+  {
+    "revision": "50590e281a4fd1bcad7f",
+    "url": "/persson/static/css/main.a127268c.chunk.css"
+  },
+  {
+    "revision": "ca65a2e65840e1239847",
+    "url": "/persson/static/js/2.b8beaf57.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/persson/static/js/2.b8beaf57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50590e281a4fd1bcad7f",
+    "url": "/persson/static/js/main.5240e88c.chunk.js"
+  },
+  {
+    "revision": "2d97abe94d3f0dc2634f",
+    "url": "/persson/static/js/runtime-main.c4af5a11.js"
+  }
+]);
